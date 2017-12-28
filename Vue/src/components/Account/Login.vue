581cc08a6653b8b19095c5e0a74eb43ebe246c6f
caisicen <caisicen@163.com>
2017-12-28 17:10:38 +0800

    更改页面高度样式

--- Vue/src/components/Account/Login.vue
+++ Vue/src/components/Account/Login.vue
@@ -1,16 +1,14 @@
 <template>
-  <el-container>
+  <el-container style="background-color: #c6d1de;height: 100%">
     <el-main>
-      <el-form label-position="left" :model="form" :rules="rules2" ref="form" label-width="80px"
-               style="margin-top: 30%">
+      <img src="../../assets/logo.png">
+      <el-form label-position="left" :model="form" :rules="rules2" ref="form" label-width="80px">
         <h3 class="title" style="margin-bottom: 20px;">VuePHP</h3>
         <el-form-item label="帐号" prop="username">
-          <el-input placeholder="请输入帐号" type="text" auto-complete="off" class="loginInput"
-                    v-model="form.username"></el-input>
+          <el-input placeholder="请输入帐号" type="text" auto-complete="off" v-model="form.username"></el-input>
         </el-form-item>
         <el-form-item label="密码" prop="password">
-          <el-input placeholder="请输入密码" type="password" auto-complete="off" class="loginInput"
-                    v-model="form.password"></el-input>
+          <el-input placeholder="请输入密码" type="password" auto-complete="off" v-model="form.password"></el-input>
         </el-form-item>
         <el-button-group style="width:100%">
           <el-button type="success" style="width:50%" @click.native.prevent="submit('form')" v-loading="loading">登录
@@ -84,12 +82,3 @@
   }
 </script>
 
-<style>
-  .loginform {
-
-  }
-
-  .loginInput {
-
-  }
-</style>
